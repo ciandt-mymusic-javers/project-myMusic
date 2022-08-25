5a88b5a87ab1ee17e095f656f3c7a1b0e6cfe8b2
BrunaAlmeida13 <bruna.a.almeida@hotmail.com>
2022-08-25 14:17:16 -0300

    Correcao da anotacao da requisicao no MusicController

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/controller/MusicController.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/controller/MusicController.java
@@ -6,10 +6,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Set;
 
@@ -24,15 +21,11 @@ public class MusicController {
         this.musicService = musicService;
     }
 
-    @Operation(summary = "test", description = "endpoint only for test")
+    @Operation(summary = "Search method for music", description = "endpoint to find music by name or artist")
     @GetMapping
-    public ResponseEntity<String> get() {
-        return ResponseEntity.ok("67f5976c-eb1e-404e-8220-2c2a8a23be47");
+    public ResponseEntity<Set<Music>> findMusicsByNameOrArtists(@RequestParam String filter) {
+        return new ResponseEntity<>(musicService.findMusicsByNameOrArtists(String.valueOf(filter)), HttpStatus.OK);
     }
 
-    @Operation(summary = "Search method for music", description = "endpoint to find music by name or artist")
-    @GetMapping(value = "/filter={name}")
-    public ResponseEntity<Set<Music>> findMusicsByNameOrArtists(@PathVariable String name) {
-        return new ResponseEntity<>(musicService.findMusicsByNameOrArtists(String.valueOf(name)), HttpStatus.OK);
-    }
+
 }
