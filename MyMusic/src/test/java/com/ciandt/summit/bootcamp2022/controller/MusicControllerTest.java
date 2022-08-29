67d15b5a99cae2190e26aac0a0767ff100b837fb
sarahp_cit <sarahp@ciandt.com>
2022-08-29 13:30:03 -0300

    Retirando imports não utilizados

--- MyMusic/src/test/java/com/ciandt/summit/bootcamp2022/controller/MusicControllerTest.java
+++ MyMusic/src/test/java/com/ciandt/summit/bootcamp2022/controller/MusicControllerTest.java
@@ -15,13 +15,12 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import java.util.*;
+import java.util.Collections;
+import java.util.HashSet;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
