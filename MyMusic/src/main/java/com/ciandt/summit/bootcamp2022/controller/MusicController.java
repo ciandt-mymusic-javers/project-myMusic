dc81921709e78aba00fe7d10f0dd0cca2ab81407
BrunaAlmeida13 <bruna.a.almeida@hotmail.com>
2022-08-31 16:07:33 -0300

    Criacao log - incompleto

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/controller/MusicController.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/controller/MusicController.java
@@ -1,9 +1,12 @@
 package com.ciandt.summit.bootcamp2022.controller;
 
+import com.ciandt.summit.bootcamp2022.SummitBootcampApplication;
 import com.ciandt.summit.bootcamp2022.entity.Music;
 import com.ciandt.summit.bootcamp2022.service.MusicService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +19,8 @@ import java.util.List;
 @RequestMapping("/api/v1/music")
 public class MusicController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(MusicController.class);
+
     @Autowired
     private final MusicService musicService;
 
@@ -25,7 +30,13 @@ public class MusicController {
             @RequestParam String filter,
             @RequestParam String pageNumber,
             @RequestParam String pageSize) {
-        return new ResponseEntity<>(musicService.findMusicsByNameOrArtists(String.valueOf(filter),
-                Integer.parseInt(pageNumber), Integer.parseInt(pageSize)).getContent(), HttpStatus.OK);
+        try {
+            LOGGER.info("Music found.");
+            return new ResponseEntity<>(musicService.findMusicsByNameOrArtists(String.valueOf(filter),
+                    Integer.parseInt(pageNumber), Integer.parseInt(pageSize)).getContent(), HttpStatus.OK);
+        } catch (RuntimeException e){
+            LOGGER.error("Error to find music.");
+            throw new RuntimeException(e.getMessage());
+        }
     }
 }
--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/service/MusicService.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/service/MusicService.java
@@ -1,9 +1,12 @@
 package com.ciandt.summit.bootcamp2022.service;
 
+import com.ciandt.summit.bootcamp2022.SummitBootcampApplication;
 import com.ciandt.summit.bootcamp2022.entity.Music;
 import com.ciandt.summit.bootcamp2022.exception.InvalidFilterSizeException;
 import com.ciandt.summit.bootcamp2022.exception.MusicNotFoundException;
 import com.ciandt.summit.bootcamp2022.repository.MusicRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -13,12 +16,16 @@ import org.springframework.stereotype.Service;
 @Service
 public class MusicService implements IMusicService{
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(MusicService.class);
+
     public static final int MINIMUM_LENGTH = 2;
     @Autowired
     private MusicRepository musicRepository;
 
     @Override
     public Page<Music> findMusicsByNameOrArtists(String filter, int pageNumber, int pageSize){
+        LOGGER.info("Music found.");
+
         if(isNotEnoughNameLength(filter))
             throw new InvalidFilterSizeException(
                     "It is not possible to filter musics with word shorter than "
