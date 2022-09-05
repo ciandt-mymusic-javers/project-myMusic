d182570be7511ecb2924b83fa8e9e5e6ddc82596
BrunaAlmeida13 <bruna.a.almeida@hotmail.com>
2022-09-05 13:07:17 -0300

    Criacao no sevice o metodo deletar musica da playlist e criacao da exception

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/repository/PlaylistRepository.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/repository/PlaylistRepository.java
@@ -2,9 +2,15 @@ package com.ciandt.summit.bootcamp2022.repository;
 
 import com.ciandt.summit.bootcamp2022.entity.Playlist;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PlaylistRepository extends JpaRepository<Playlist, String> {
+    @Query(value = "SELECT MusicaId as musicId FROM PlaylistMusicas WHERE PlaylistId =:playlistId AND MusicaId =:musicId", nativeQuery = true)
+    String findMusicIntoPlaylist(@Param("playlistId") String playlistId, @Param("musicId") String musicId);
 
+    @Query(value = "DELETE FROM PlaylistMusicas WHERE PlaylistId =:playlistId AND MusicaId =:musicId", nativeQuery = true)
+    Integer deleteMusicOfPlaylist(@Param("playlistId") String playlistId, @Param("musicId") String musicId);
 }
