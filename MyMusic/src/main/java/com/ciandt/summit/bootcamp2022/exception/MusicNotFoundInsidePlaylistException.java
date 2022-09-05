29a834654639fbe88285fd1b3f36c959a35e287c
BrunaAlmeida13 <bruna.a.almeida@hotmail.com>
2022-09-05 13:04:43 -0300

    Criacao no sevice o metodo deletar musica da playlist e criacao da exception

---
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/exception/MusicNotFoundInsidePlaylistException.java
@@ -0,0 +1,6 @@
+package com.ciandt.summit.bootcamp2022.exception;
+
+public class MusicNotFoundInsidePlaylistException extends Throwable {
+    public MusicNotFoundInsidePlaylistException(String s) {
+    }
+}
