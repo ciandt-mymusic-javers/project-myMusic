843fd606688f2c094e0686062ff604abdf0a3af4
José Raimundo <joseff@ciandt.com>
2022-09-05 15:06:06 -0300

    corrigido url no playlistcontrollerTest

--- MyMusic/src/test/java/com/ciandt/summit/bootcamp2022/controller/PlaylistControllerTest.java
+++ MyMusic/src/test/java/com/ciandt/summit/bootcamp2022/controller/PlaylistControllerTest.java
@@ -54,7 +54,7 @@ class PlaylistControllerTest {
         given(playlistService.addMusicIntoPlaylist(Mockito.any(Music.class), anyString()))
                 .willThrow(new MusicOrPlaylistNotFoundException("Not found"));
 
-        String url = "/api/playlists/1/musics";
+        String url = "/api/v1/playlist/1/musics";
 
         String body = String.valueOf(music);
 
@@ -69,7 +69,7 @@ class PlaylistControllerTest {
     @DisplayName("Add music into playlist should return HTTP.StatusCode.OK")
     void addMusicIntoPlaylistSuccess() throws Exception {
 
-        String url = "/api/playlists/1/musics";
+        String url = "/api/v1/playlist/1/musics";
 
         String body = "{\n" +
                 "     \"id\": \"4ffb5d4f-8b7f-4996-b84b-ecf75.1f52eea\",\n" +
